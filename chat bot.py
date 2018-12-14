ed2ec8e59cc6452da045401d9ede070fc3af8777
Themlghusky <45860408+Themlghusky@users.noreply.github.com>
2018-12-14 15:03:30 -0600

    Add files via upload

---
+++ chat bot.py
@@ -0,0 +1,32 @@
+print("hello whats your name ")
+awnser = raw_input(" ")
+print("nice to meet you", awnser ,"my name is chat bot")
+print("have you had a good day? yes or no")
+response = raw_input(" ")
+if response== ("yes"):
+    print "wonderful"
+else:
+    print "ok"
+if response== "yes":
+    print "what will you do whan you get home today"    
+if response== "yes":
+    response = thing = raw_input(" ")
+    if response== thing:
+        print("thats nice")
+if response== "no":
+    print("how will you make it better")
+    if response == "no":
+        response = how = raw_input(" ")
+        if response== how:
+            print(" that sounds wonderful")
+print("what gender are you boy, girl, other")           
+if thing== raw_input(" "):
+    thing = yeet = raw_input(" ")
+    if yeet== ("boy"):
+        print("thats nice sir")
+    elif yeet== "girl":
+        print("ok mam")
+    elif yeet== "other"
+        print("ok, you be you")
+    else:
+        print('not telling eh')   
